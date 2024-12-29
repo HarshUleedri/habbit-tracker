72ca2efe38f1a37ed38ec9fc0a9d82ee434f689b
HarshUleedri <ulidari2@gmail.com>
2024-12-29 15:08:48 +0530

    feat: :sparkles: ui added for a case which is if habits are not there then it will show add your habit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,16 +3,25 @@ import HabitCard from "./component/HabitCard";
 import CreateHabit from "./component/CreateHabit";
 const Home = () => {
   const habits = useSelector((state) => state.habits);
+  console.log(habits);
   return (
     <>
-      <div className="max-w-5xl py-4 mx-auto">
+      <div
+        className={`${
+          habits.length === 0 ? "h-[100vh]" : "h-full"
+        } max-w-5xl py-4 mx-auto`}
+      >
         <div className="mb-12">
           <CreateHabit />
         </div>
         <hr className="my-12 border-t-2 border-zinc-600" />
-        {habits.map((habit) => (
-          <HabitCard key={habit.id} habit={habit} />
-        ))}
+        {habits.length > 0 ? (
+          habits.map((habit) => <HabitCard key={habit.id} habit={habit} />)
+        ) : (
+          <p className="text-4xl text-center capitalize text-zinc-300">
+            Add your first habit
+          </p>
+        )}
       </div>
     </>
   );
